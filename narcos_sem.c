cb394b106f44c4aaa543fbf96e4e4913cd06af39
DanielBarragan96 <ie702554@iteso.mx>
2018-03-11 09:14:02 -0600

    sem w/errors

--- narcos_sem.c
+++ narcos_sem.c
@@ -13,27 +13,40 @@
 
 #define CICLOS 10
 
-typedef int* Semaphore;
+typedef struct
+{
+	int max_val;
+	int* value;
+}Semaphore;
+
 
 char *pais[3]={"Peru","Bolvia","Colombia"};
-Semaphore sem;
 
 void initsem(Semaphore checkSem, int value)
 {
-
+	*checkSem.value = checkSem.max_val;
+	checkSem.max_val = value;
 }
 
 void waitsem(Semaphore checkSem)
 {
-	
+	int l=0;
+	//do { atomic_xchg(l,*sem.value); } while(l<=0);
+	while(l > *checkSem.value)
+	{
+	}
+	*checkSem.value -= 1;
 }
 
 void signalsem(Semaphore checkSem)
 {
-	
+	if(checkSem.max_val > *checkSem.value)
+	{
+		*checkSem.value += 1;		
+	}
 }
 
-void proceso(int i)
+void proceso(Semaphore sem, int i)
 {
 	int k;
 	for(k=0;k<CICLOS;k++)
@@ -44,7 +57,7 @@ void proceso(int i)
 		fflush(stdout);
 		sleep(rand()%3);
 		printf("- %s Sale\n",pais[i]);
-		// Llamada waitsignal implementada en la parte 3
+		// Llamada signalsem implementada en la parte 3
 		signalsem(sem);
 		// Espera aleatoria fuera de la sección crítica
 		sleep(rand()%3);
@@ -54,6 +67,7 @@ void proceso(int i)
 
 int main()
 {
+	Semaphore sem;
 	int pid;
 	int status;
 	int shmid;
@@ -61,26 +75,30 @@ int main()
 	
 	// Solicitar memoria compartida
 	shmid=shmget(0x1234,sizeof(sem),0666|IPC_CREAT);
+	
 	if(shmid==-1)
 	{
 		perror("Error en la memoria compartida\n");
 		exit(1);
 	}
+	
 	// Conectar la variable a la memoria compartida
-	sem=shmat(shmid,NULL,0);
-	if(sem==NULL)
+	int* shm_ptr = (int *) shmat(shmid,NULL,0);
+	
+	if(shm_ptr==NULL)
 	{
 		perror("Error en el shmat\n");
 		exit(2);
 	}
-	*sem=0;
+	
+	initsem(sem,0);//inicia en cero por que no está tomado
 	
 	for(int i=0;i<3;i++)
 	{
 		// Crea un nuevo proceso hijo que ejecuta la función proceso()
 		pid=fork();
 		if(pid==0)
-		proceso(i);
+			proceso(sem, i);
 	}
 	for(int i=0;i<3;i++)
 		pid = wait(&status);
