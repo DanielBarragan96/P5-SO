7439e747f31ab10e2024cb25565e56607446259e
santiago <31044042+SantSou@users.noreply.github.com>
2018-03-15 09:58:11 -0600

    fixed bug
    now all processes use the actual shared memory, still doesnt work

--- narcos_sem.c
+++ narcos_sem.c
@@ -18,7 +18,10 @@ typedef struct
 	int max_val;
 	int* value;
 }Semaphore;
+	
 
+	int shmid;
+	float* shm;
 
 char *pais[3]={"Peru","Bolvia","Colombia"};
 
@@ -27,38 +30,49 @@ void initsem(Semaphore* checkSem, int value)
 	printf("\nprueba init %d\n",value);
 	checkSem->max_val = value;
 	printf("\nprueba init 2\n");
-	checkSem->max_val = *checkSem->value;
-	printf("FIN init\n");
+	*checkSem->value = checkSem->max_val;
+	printf("FIN init %d\n",checkSem->max_val);
 }
 
-void waitsem(Semaphore* checkSem)
+void waitsem(Semaphore* checkSem, float* shm)
 {
 	int l=0;
 	//do { atomic_xchg(l,*sem.value); } while(l<=0);
+	*checkSem->value = *shm;
 	while(l > *checkSem->value);
 	*checkSem->value -= 1;
+	*shm=*checkSem->value;
 }
 
 void signalsem(Semaphore* checkSem)
 {
 	if(checkSem->max_val > *checkSem->value)
 	{
-		*checkSem->value += 1;		
+		*checkSem->value += 1;	
+		*shm=(float)*checkSem->value;	
 	}
 }
 
 void proceso(Semaphore* sem, int i)
 {
-	int k;
+	int k;	
+	// Conectar la variable a la memoria compartida
+	shm = shmat(shmid,NULL,0);
+	
+	if(shm==NULL)
+	{
+		perror("Error en el shmat\n");
+		exit(2);
+	}
+
 	for(k=0;k<CICLOS;k++)
 	{
-		// Llamada waitsem implementada en la parte 3
-		waitsem(sem);
+		printf("\nproceso %d con %s y sema: %f\n",i,pais[i],*shm);
+		waitsem(sem,shm);
 		printf("Entra %s ",pais[i]);
 		fflush(stdout);
 		sleep(rand()%3);
 		printf("- %s Sale\n",pais[i]);
-		// Llamada signalsem implementada en la parte 3
 		signalsem(sem);
 		// Espera aleatoria fuera de la sección crítica
 		sleep(rand()%3);
@@ -73,10 +87,8 @@ int main()
 	Semaphore* sem = &sema;
 	int pid;
 	int status;
-	int shmid;
-	float* shm;
+
 	int args[3];
-	
 	// Solicitar memoria compartida
 	shmid=shmget(0x1234,sizeof(Semaphore),0666|IPC_CREAT);
 	
@@ -85,19 +97,6 @@ int main()
 		perror("Error en la memoria compartida\n");
 		exit(1);
 	}
-	
-	// Conectar la variable a la memoria compartida
-	printf("prueba1 \n");
-	shm = shmat(shmid,NULL,0);
-	printf("prueba 1.5 \n");
-	//printf("prueba 2: %f",*shm);
-
-	if(shm==NULL)
-	{
-		perror("Error en el shmat\n");
-		exit(2);
-	}
-	
 	initsem(sem,0);//inicia en cero por que no está tomado
 	
 	for(int i=0;i<3;i++)
@@ -105,7 +104,7 @@ int main()
 		// Crea un nuevo proceso hijo que ejecuta la función proceso()
 		pid=fork();
 		if(pid==0)
-			proceso(sem, i);
+		proceso(sem, i);
 	}
 	for(int i=0;i<3;i++)
 		pid = wait(&status);
